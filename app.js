f286418ebcd3496bc19006fb88d650ee10288686
saqlainmujtaba <hafizsaqlainmujtaba@gmail.com>
2025-08-12 22:26:20 +0500

    home page added

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const app = express();
 
@@ -18,33 +24,17 @@ app.use(
   })
 );
 // app.use(cors());
-app.use(cors({
-  origin: [
-    'http://localhost:19006',
-    'exp://192.168.10.20:19000',
-    'exp://192.168.10.20:8081'
-  ],
-  credentials: true,
-  exposedHeaders: ['set-cookie']
-}));
+app.use(
+  cors({
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
 
 app.use("/api/v1", User);
 
 app.get("/", (req, res) => {
-  res.send("Server is working");
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+  // res.send("Server is working");
 });
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(
-//   fileUpload({
-//     limits: { fileSize: 50 * 1024 * 1024 },
-//     useTempFiles: true,
-//   })
-// );
-
-// app.use("/api/v1", User);
-// // here  "/api/v1" is the prefix to every route
--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 import { app } from "./app.js";
 import { config } from "dotenv";
 import { connectDatabase } from "./config/database.js";
-// import cloudinary from "cloudinary/v2"
-// Require the cloudinary library
-// const cloudinary = require("cloudinary").v2;
-import { v2 as cloudinary } from 'cloudinary';
 
 config({
   path: "./config/.env",
 });
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET,
-// });
-
-// Return "https" URLs by setting secure: true
-cloudinary.config({
-      secure: true
-    });
-    
-    // Log the configuration
-    console.log(cloudinary.config());
 
 connectDatabase();
 app.listen(process.env.PORT, () => {
